5c545cb50620dcde50140207d4189f8b4ded5450
JMGarCas <jmgarciacasillas135@gmail.com>
2024-07-15 19:33:26 +0200

    feat: initial configuration TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+
+module.exports = tseslint.config(
+  { ignores: ["*.js", "dist"] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+);
