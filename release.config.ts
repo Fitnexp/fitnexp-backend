d59ed29c59b004f8d6e9deaf1ced85bc6e4f8507
JMGarCas <jmgarciacasillas135@gmail.com>
2024-07-21 20:19:39 +0200

    ci: New Github Action for automatic releases

--- release.config.ts
+++ release.config.ts
@@ -3,4 +3,17 @@
  */
 export default {
     branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['dist/*'],
+                message:
+                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+        '@semantic-release/github',
+    ],
 };
