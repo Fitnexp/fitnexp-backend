21fcb2281110c76a73f160f2ca4187966911bb77
JMGarCas <jmgarciacasillas135@gmail.com>
2024-07-21 18:38:58 +0200

    style: Format code

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 // @ts-check
 
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
 
 module.exports = tseslint.config(
-  { ignores: ["*.js", "dist"] },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+    { ignores: ['*.js', 'dist'] },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
 );
