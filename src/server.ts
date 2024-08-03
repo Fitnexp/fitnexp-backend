e710ee6d97c203913bae58625203cda5a9e01d10
JMGarCas <jmgarciacasillas135@gmail.com>
2024-08-03 19:56:59 +0200

    chore: limit cors allowed origins to just localhost

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import cors from 'cors';
 import indexRoutes from './indexRoutes';
 import bodyParser from 'body-parser';
 
+const corsOptions = {
+    origin: /^http:\/\/localhost:\d+$/,
+};
 function createServer() {
     const app = express();
-    app.use(cors());
+    app.use(cors(corsOptions));
     app.use(express.json());
     app.use(bodyParser.json());
     app.use('/api', indexRoutes);
