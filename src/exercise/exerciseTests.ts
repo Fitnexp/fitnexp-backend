abf11194c9b5cc1f5b8503cad5c3aef77a323ad0
JMGarCas <jmgarciacasillas135@gmail.com>
2024-08-18 12:43:45 +0200

    chore: avoid duplication

--- src/exercise/exerciseTests.ts
+++ src/exercise/exerciseTests.ts
@@ -1,31 +1,21 @@
-import supertest from 'supertest';
 import { Express } from 'express';
+import { logUser, retrieveData } from '../utils/testsUtils';
 
 function exerciseTests(app: Express) {
     describe('Exercise', () => {
         let cookie = '';
 
         beforeAll(async () => {
-            const responseLogUser = await supertest(app)
-                .post('/api/login')
-                .send({
-                    email: 'alberto@gmail.com',
-                    password: 'passwordpassword', // NOSONAR,
-                })
-                .expect(200);
-
-            cookie = responseLogUser.headers['set-cookie'];
+            cookie = await logUser(app);
         });
 
         describe('when the user retrieves all exercises', () => {
             it('should return all exercises', async () => {
-                const response = await supertest
-                    .agent(app)
-                    .get('/api/exercises')
-                    .set('Cookie', cookie)
-                    .send();
-
-                expect(response.status).toBe(200);
+                const response = await retrieveData(
+                    app,
+                    cookie,
+                    '/api/exercises',
+                );
                 expect(response.body.exercises.length).toBe(873);
             });
         });
--- src/workout/workoutTests.ts
+++ src/workout/workoutTests.ts
@@ -1,31 +1,21 @@
-import supertest from 'supertest';
 import { Express } from 'express';
+import { logUser, retrieveData } from '../utils/testsUtils';
 
 function workoutTests(app: Express) {
     describe('Workout', () => {
         let cookie = '';
 
         beforeAll(async () => {
-            const responseLogUser = await supertest(app)
-                .post('/api/login')
-                .send({
-                    email: 'alberto@gmail.com',
-                    password: 'passwordpassword', // NOSONAR,
-                })
-                .expect(200);
-
-            cookie = responseLogUser.headers['set-cookie'];
+            cookie = await logUser(app);
         });
 
         describe('when the user retrieves all workouts', () => {
             it('should return all workouts related to that user', async () => {
-                const response = await supertest
-                    .agent(app)
-                    .get('/api/workouts')
-                    .set('Cookie', cookie)
-                    .send();
-
-                expect(response.status).toBe(200);
+                const response = await retrieveData(
+                    app,
+                    cookie,
+                    '/api/workouts',
+                );
                 expect(response.body.workouts.length).toBe(2);
             });
         });
